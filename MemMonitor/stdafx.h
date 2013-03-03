476e7bf1cbf967c5d2fb03eb9eaf2a2f46c8c117
jjuiddong <jjuiddong@hanmail.net>
2013-03-02 22:33:36 -0800

    update build warning error

--- MemMonitor/stdafx.h
+++ MemMonitor/stdafx.h
@@ -19,6 +19,7 @@
 
 // MFC의 공통 부분과 무시 가능한 경고 메시지에 대한 숨기기를 해제합니다.
 #define _AFX_ALL_WARNINGS
+#define _INTSAFE_H_INCLUDED_
 
 #include <afxwin.h>         // MFC 핵심 및 표준 구성 요소입니다.
 #include <afxext.h>         // MFC 확장입니다.
